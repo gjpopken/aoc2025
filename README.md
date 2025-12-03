0d7468533e5dcb41efcc6502ad890d475d4af0a0
Gavin Popken <gavinpopkenart@gmail.com>
2025-12-03 17:20:06 -0600

    finished day 3 part one and start part 2

--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
-![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%86/yellow)](src/day03)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
@@ -69,9 +69,9 @@ npm start 1
 
 ```
 Day 01
-Time part 1: 0.754ms
-Time part 2: 22.567ms
-Both parts: 23.321ms
+Time part 1: 0.729ms
+Time part 2: 51.059ms
+Both parts: 51.788ms
 ```
 
 ```
@@ -83,9 +83,9 @@ Both parts: 2844.407ms
 
 ```
 Day 03
-Time part 1: -
+Time part 1: 1.256ms
 Time part 2: -
-Both parts: -
+Both parts: 1.256ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 4/50
-Total time: 2867.729ms
+Total stars: 5/50
+Total time: 2897.45ms
 ```
 
 <!--/RESULTS-->
--- src/day03/index.ts
+++ src/day03/index.ts
@@ -2,41 +2,83 @@ import run from "aocrunner"
 
 const parseInput = (rawInput: string) => rawInput.split("\n")
 
+const testInput1 = `987654321111111
+811111111111119
+234234234234278
+818181911112111`
+
+const testInput2 = "3332221732222313632231112123312334132423312333223376521312228322222132232231322233613312223323133232"
+
 const part1 = (rawInput: string) => {
-  const input = parseInput(rawInput)
-  console.log(input);
-  
+  const input = parseInput(rawInput);
+  let joltage = 0
   input.forEach(bank => {
-    bank.split("").map(bat => +bat)
+    const batteries = bank.split("").map(bat => +bat)
+    let highestNumber = 0
+    let index = 0
+    batteries.forEach((bat, i, array) => {
+      const length = array.length
+      if (bat > highestNumber && i !== length - 1) {
+        highestNumber = bat
+        index = i
+      }
+    })
+    const restOfTheArray = batteries.splice((index || 0) + 1)
+    const nextHighest = restOfTheArray.sort((a, b) => b - a)[0]
+    joltage += Number([highestNumber, nextHighest].join(""))
   })
-  return
+  return joltage
 }
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput)
+  const input = parseInput(testInput1)
+  let joltage = 0
+  input.forEach(bank => {
+    joltage += getMaxJoltage(bank)
+  })
 
-  return
+  return joltage
+}
+
+const getMaxJoltage = (bank: string): number => {
+  const batteries = bank.split("").map(bat => +bat)
+  const batteriesOn: number[] = []
+  for (let i = 12; i > 0; i--) {
+    let highestNumber = 0
+    batteries.forEach((bat, index, array) => {
+      const length = array.length
+      if (bat > highestNumber && index < length - i) {
+        highestNumber = bat
+      }
+    })
+    batteriesOn.push(highestNumber)
+  }
+  console.log("Highest numbers:", batteriesOn);
+  
+  const totalSum = batteriesOn.reduce((acc, curr) => curr + acc, 0)
+  return totalSum
 }
 
 run({
   part1: {
     tests: [
       {
-        input: `987654321111111
-811111111111119
-234234234234278
-818181911112111`,
+        input: testInput1,
         expected: 357,
       },
+      {
+        input: testInput2,
+        expected: 86
+      }
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: testInput1,
+        expected: 3121910778619,
+      },
     ],
     solution: part2,
   },
