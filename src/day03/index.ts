1b01703f89d82268047b953bcd91003e5eb93574
Gavin Popken <gavinpopkenart@gmail.com>
2025-12-03 08:20:52 -0600

    start day 3

---
+++ src/day03/index.ts
@@ -0,0 +1,45 @@
+import run from "aocrunner"
+
+const parseInput = (rawInput: string) => rawInput.split("\n")
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput)
+  console.log(input);
+  
+  input.forEach(bank => {
+    bank.split("").map(bat => +bat)
+  })
+  return
+}
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput)
+
+  return
+}
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `987654321111111
+811111111111119
+234234234234278
+818181911112111`,
+        expected: 357,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+})
